6a2c961abce0651c2fcee8eb4a67f23b11d030e3
joypythondev <ATBBNq2rv3Ugrv5tMc77dbP6QQS2CCE2C891>
2026-06-08 13:57:17 +0600

    CSRF_TRUSTED_ORIGINS

--- dropshipping_project_app/settings.py
+++ dropshipping_project_app/settings.py
@@ -37,6 +37,7 @@ DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split()
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "").split()
 
 from corsheaders.defaults import default_headers
 
